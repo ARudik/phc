ada04e20b993222f948e917b9643a3e60fde3d2f
edskodevries <edskodevries@bbcf20a9-8526-0410-90e2-9b3d3f589b1d>
2007-03-27 21:30:21 +0000

    Removing the old trunk/, moving up branches/ and tags/

--- phc/trunk/test/subjects/codegen/021a.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$a = 1;
-	$b = $a;
-	debug_zval_dump($b);
-?>
--- phc/trunk/test/subjects/general/prepostop_error1.php
+++
@@ -1,6 +0,0 @@
-<?php
-	// error: tests/subjects/general/prepostop_error1.php:5: error: Variable name expected
-	// return: 2
-
-	f()++;
-?>
